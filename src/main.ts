4ce1a02fc05e89a0a39cd1fdfe1a4d3847a5b81d
Renan C P <renan.coelho.p@gmail.com>
2025-06-26 14:39:02 -0300

    [FEAT] Helmet

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwaggerPlugin } from './config/swagger.config';
 import { setupWinstonLogger } from './config/winston.config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: setupWinstonLogger(),
   });
+
+  app.use(helmet());
 
   app.useGlobalPipes(
     new ValidationPipe({
